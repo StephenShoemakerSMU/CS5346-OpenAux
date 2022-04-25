b9325465ef824a722a69915d21410e8c753f7096
Sol1taire <chrisswood1024@protonmail.com>
2022-04-24 21:44:17 -0500

    Updated icecast config

--- icecast/config/run.sh
+++ icecast/config/run.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 if [ -n "$ICECAST_SOURCE_PASS" ]; then
-	echo "Source password confirmed"
     sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>$ICECAST_SOURCE_PASS<\/source-password>/g" /etc/icecast2/icecast.xml
+else
+	echo "ERROR: No source password received"
 fi
+
 if [ -n "$ICECAST_RELAY_PASS" ]; then
-	echo "Relay password confirmed"
     sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>$ICECAST_RELAY_PASS<\/relay-password>/g" /etc/icecast2/icecast.xml
+else
+	echo "ERROR: No relay password received"
 fi
+
 if [ -n "$ICECAST_ADMIN_PASS" ]; then
-	echo "Admin password confirmed"
     sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>$ICECAST_ADMIN_PASS<\/admin-password>/g" /etc/icecast2/icecast.xml
+else
+	echo "ERROR: No admin password received"
 fi
 
 /etc/init.d/icecast2 start && tail -F /var/log/icecast2/access.log
